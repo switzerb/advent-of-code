4f260b5e0152b4709f3dd19451c51915ac7ece5c
bswitzer <brenna.switzer@gmail.com>
2025-12-30 17:22:30 -0800

    day two slightly less awful

--- haskell/src/Y2025D02.hs
+++ haskell/src/Y2025D02.hs
@@ -5,67 +5,40 @@ import Data.List.Split (splitOn)
 type Range = (Int, Int)
 
 parse :: String -> [Range]
-parse input = map parseRange (splitOn "," input)
+parse = map parseRange . splitOn ","
   where
-    parseRange s = 
-      let parts = splitOn "-" s
-          start = read (trim (parts !! 0)) :: Int
-          end = read (trim (parts !! 1)) :: Int
-      in (start, end)
-    trim = filter (/= '\n') . dropWhile (== ' ') . reverse . dropWhile (== ' ') . reverse
+    parseRange s = case splitOn "-" s of
+      [start, end] -> (read (trim start), read (trim end))
+      _ -> error "Invalid range format"
+    trim = filter (`notElem` ['\n', ' '])
 
 everyItemEqual :: String -> Bool
-everyItemEqual digits
-  | length digits < 2 = False
-  | otherwise = all (== head digits) digits
+everyItemEqual digits = length digits >= 2 && all (== head digits) digits
 
 isInvalid :: Int -> Bool
 isInvalid n =
   let digits = show n
       len = length digits
-  in if len `mod` 2 /= 0
-     then False
-     else if everyItemEqual digits
-          then True
-          else let mid = len `div` 2
-                   firstHalf = take mid digits
-                   secondHalf = drop mid digits
-               in firstHalf == secondHalf
+  in even len && (everyItemEqual digits || halvesEqual digits len)
+  where
+    halvesEqual ds l = take (l `div` 2) ds == drop (l `div` 2) ds
 
 chunk :: Int -> [a] -> [[a]]
 chunk _ [] = []
 chunk n xs = take n xs : chunk n (drop n xs)
 
 allArraysEqual :: Eq a => [[a]] -> Bool
-allArraysEqual [] = True
-allArraysEqual [_] = True
 allArraysEqual (x:xs) = all (== x) xs
+allArraysEqual _ = True
 
 isMoreInvalid :: Int -> Bool
 isMoreInvalid n =
   let digits = show n
-  in if everyItemEqual digits
-     then True
-     else let maxChunkSize = length digits `div` 2
-              options = [2..maxChunkSize]
-          in any (\option -> 
-                let arrays = chunk option digits
-                in allArraysEqual arrays) options
-
-sumOfInvalidIds :: [Int] -> (Int -> Bool) -> Int
-sumOfInvalidIds items isInvalidFunc = 
-  sum $ filter isInvalidFunc items
-
-buildRanges :: Range -> [Int]
-buildRanges (start, end) = [start..end]
+  in everyItemEqual digits || any (allArraysEqual . (`chunk` digits)) [2..length digits `div` 2]
 
 partOne :: String -> Int
-partOne input = 
-  sum $ map (\range -> 
-    sumOfInvalidIds (buildRanges range) isInvalid) (parse input)
+partOne = sum . concatMap (filter isInvalid . uncurry enumFromTo) . parse
 
 partTwo :: String -> Int
-partTwo input = 
-  sum $ map (\range -> 
-    sumOfInvalidIds (buildRanges range) isMoreInvalid) (parse input)
+partTwo = sum . concatMap (filter isMoreInvalid . uncurry enumFromTo) . parse
 
